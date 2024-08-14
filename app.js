d4856a30219460e7872d4ebfe9d42524d9ab42ca
IhorZolot <fedonn@ukr.net>
2024-08-14 22:57:58 +0300

    update

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from 'express'
 import 'dotenv/config'
 
 const app = express()
-
-app.get('/', (request, response) => {
-	response.send('<h1>Home page</h1>')
+app.use((req, res, next) => {
+	console.log('Access-Control-Allow-Origin')
 })
-app.get('/contacts', (request, response) => {
-	response.send('<h1>Contacts page</h1>')
+
+app.get('/', (req, res) => {
+	res.json([])
+})
+app.get('/contacts', (req, res) => {
+	res.send('<h1>Contacts page</h1>')
 })
 
 export default app
