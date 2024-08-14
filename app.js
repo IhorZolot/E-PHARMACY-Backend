2238343727cf133f9708e3afd2d314a65454167f
IhorZolot <fedonn@ukr.net>
2024-08-14 22:17:44 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 import express from 'express'
-// import logger from 'morgan'
-import cors from 'cors'
 import 'dotenv/config'
 
-// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-// app.use(logger(formatsLogger))
-// app.use(cors())
-// app.use(express.json())
 const app = express()
 
 app.get('/', (request, response) => {
