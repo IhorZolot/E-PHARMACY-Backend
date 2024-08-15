18dee50a6ca3c26d50ab695b9b5df3aebbdf47a5
IhorZolot <fedonn@ukr.net>
2024-08-15 22:31:35 +0300

    update server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 mongoose
 	.connect(process.env.DB_HOST)
 	.then(() => {
-		app.listen(3000, () => {
+		app.listen(PORT, () => {
 			console.log(`Server running. Use our API on port:${PORT}`)
 		})
 	})
