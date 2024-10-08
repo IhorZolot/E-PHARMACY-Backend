313c251c87f61fb8bdb662a65307a555dc8c007a
IhorZolot <fedonn@ukr.net>
2024-10-08 23:50:01 +0300

    add user schema phone number

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ import Joi from 'joi'
 
 import { handleSaveError, preUpdate } from '../hooks/hooks.js'
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const phoneRegexp = /^\+?\d{1,4}?[-.\s]?(\(?\d{1,3}?\)?[-.\s]?)?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
 
 const userSchema = new Schema(
   {
@@ -13,6 +14,12 @@ const userSchema = new Schema(
     email: {
       type: String,
       match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: String, 
+      match: phoneRegexp, 
       unique: true,
       required: true,
     },
@@ -34,6 +41,7 @@ userSchema.post('findByIdAndUpdate', handleSaveError)
 export const userSignupSchema = Joi.object({
   username: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
+  phone: Joi.string().pattern(phoneRegexp).required(),
   password: Joi.string().min(3).required(),
 })
 
